281fe04e39717fd412a0bd66b44845fbebdfceb9
Boryhit <mateusvog@gmail.com>
2026-07-23 21:09:20 -0300

    atualizando a routa de personagens

--- src/models/character.model.js
+++ src/models/character.model.js
@@ -7,21 +7,27 @@ const characterSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },   
-   ownerId: {
+   userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-    email: {
-      type: mongoose.Schema.Types.String,
-      required: "User",
-      unique: true,
+    tableId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Table",
+      required: true,
     },
-    password: {
-      type: mongoose.Schema.Types.String,
-      ref: "User",
-      required: "true",
+    classe: {
+      type: String,
+      required: true,
+      trim: true,
     },
+    race: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
 }, { timestamps: true });
 
 const Character = mongoose.model('Character', characterSchema);
